ad95efd0129fcae635d4d254dec287f3dd2b05c2
Paulo Henrique Ortolan <portolan@mercadona.com>
2026-04-08 13:13:13 +0100

    Initial commit

---
+++ src/main/java/pt/pauloortolan/plm_back/PlmBackApplication.java
@@ -0,0 +1,13 @@
+package pt.pauloortolan.plm_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlmBackApplication.class, args);
+	}
+
+}
